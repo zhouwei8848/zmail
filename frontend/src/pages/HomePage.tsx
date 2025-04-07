e6cefdf0ae606ee928fff8af2cb457e32f00ed12
zhouwei8848 <245088161@qq.com>
2025-04-07 15:52:54 +0800

    Update HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -9,7 +9,7 @@ const StructuredData: React.FC = () => {
   const jsonLd = {
     "@context": "https://schema.org",
     "@type": "WebApplication",
-    "name": "ZMAIL-24小时匿名邮箱",
+    "name": "DJSMAIL-24小时匿名邮箱",
     "applicationCategory": "UtilityApplication",
     "operatingSystem": "All",
     "offers": {
